38a4d9831269772592f2331e72d3e891238752f7
unknown <oluwadareseyii@gmail.com>
2020-11-09 09:17:42 +0100

    initial commit

---
+++ migrations/001-helloworld.sql
@@ -0,0 +1,13 @@
+-- UP
+CREATE TABLE Person (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT,
+    email TEXT
+)
+
+CREATE TABLE Vehicle (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT,
+    email TEXT
+)
+-- Down
